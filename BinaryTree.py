a0e1e59cc9d4619c062cc2ad05043e0808b7c31f
Luis Antonio Momm Duarte <luismomm@gmail.com>
2021-11-26 15:24:05 -0300

    Convert to posfix (#3)
    * init convertor
    
    * stack not working
    
    * finish posfix translator with text
    
    * refactor stack in its own file

--- BinaryTree.py
+++ BinaryTree.py
@@ -3,4 +3,21 @@ class BinaryTree():
         self.left = None
         self.right = None
         self.value = value
-        self.operator = False
+
+    def create_tree_from_tokens(token_list):
+        tree = BinaryTree('')
+        stack = []
+        stack.append(tree)
+        current_tree = tree
+
+        for i in token_list:
+            if not is_operator(i):
+                current_tree.left = ''
+                stack.append(current_tree)
+                current_tree = current_tree.left
+
+        return tree
+
+    def is_operator(token):
+        operator = ["+", "-", "*", "-"]
+        return token in operator
---
+++ convertToPosfix.py
@@ -0,0 +1,38 @@
+from Stack import *
+
+class ConvertorToPosfix:
+    def __init__(self, token_list):
+        self.stack = Stack()
+        self.output = []
+        self.precedence = {'(' : 1, '+': 2, '-': 2, '*': 3, '/': 3}
+        self.token_list = token_list
+
+    def _process_subexpression(self):
+                top_token = self.stack.pop()
+                while top_token != '(':
+                    self.output.append(top_token)
+                    top_token = self.stack.pop()
+                
+    def translate_infix_to_posfix(self):
+        for token in self.token_list:
+            if token.isnumeric():
+                self.output.append(token)
+            elif token == '(':
+                self.stack.push(token)
+            elif token == ')':
+                self._process_subexpression()
+            else:
+                while (not self.stack.empty()) and (self.precedence[self.stack.peek()] >= self.precedence[token]):
+                    self.output.append(self.stack.pop())
+                self.stack.push(token)
+
+        while not self.stack.empty():
+            self.output.append(self.stack.pop())
+
+        return self.output
+            
+               
+
+
+
+    
