2d0bafc2ed14d1a69bb4db875ebd7b07da013670
Luis Antonio Momm Duarte <luismomm@gmail.com>
2021-11-28 16:33:52 -0300

    Create evaluator funcion

---
+++ EvaluatePosfix.py
@@ -0,0 +1,35 @@
+from pdb import set_trace
+from Stack import *
+
+
+class EvaluatePostFix:
+    def __init__(self, expression):
+        self.stack = Stack()
+        self.expression = expression
+        self.OPERATORS = set(['+', '-', '*', '/'])
+
+    def evaluate_posfix(self):
+        if len(self.expression) == 0:
+            return str(self.stack.peek())
+
+        if self.expression[0] not in self.OPERATORS:
+            self.stack.push(self.expression[0])
+            self.expression = self.expression[1:]
+            return self.evaluate_posfix()
+        else:
+            first_operand = int(self.stack.pop())
+            second_operand = int(self.stack.pop())
+
+            if self.expression[0] == '+':
+                result = first_operand + second_operand
+            elif self.expression[0] == '-':
+                result = second_operand - first_operand
+            elif self.expression[0] == '*':
+                result = first_operand * second_operand
+            elif self.expression[0] == '/':
+                result = second_operand / first_operand
+
+            self.stack.push(result)
+            self.expression = self.expression[1:]
+
+            return self.evaluate_posfix()
--- Stack.py
+++ Stack.py
@@ -1,6 +1,6 @@
-class Stack():
+class Stack:
     def __init__(self):
-        self.stack = [] 
+        self.stack = []
 
     def push(self, data):
         self.stack.append(data)
@@ -8,14 +8,14 @@ class Stack():
     def pop(self):
         if self.stack:
             return self.stack.pop()
-        else: 
+        else:
             return None
 
     def size(self):
         return len(self.stack)
 
     def empty(self):
-        return True if self.size() == 0 else False 
+        return True if self.size() == 0 else False
 
     def peek(self):
         return self.stack[-1]
